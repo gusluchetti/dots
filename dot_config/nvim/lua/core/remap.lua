3f9ef184abadb38ac710dd2ff482b5782e5d070e
Gustavo Luchetti <gustavo@luchetti.dev>
2023-05-31 15:33:05 -0300

    Update .config/nvim/lua/core/remap.lua

--- dot_config/nvim/lua/core/remap.lua
+++ dot_config/nvim/lua/core/remap.lua
@@ -28,7 +28,3 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
