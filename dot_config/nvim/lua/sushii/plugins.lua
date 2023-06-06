ba4e139c3e47edf4dcda76a3d408a002df5c4a18
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-06 12:02:24 -0300

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/sushii/plugins.lua

--- dot_config/nvim/lua/sushii/plugins.lua
+++ dot_config/nvim/lua/sushii/plugins.lua
@@ -1,19 +1,20 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	{"ellisonleao/gruvbox.nvim", priority = 1000},
-    {"folke/which-key.nvim",
+    { "ellisonleao/gruvbox.nvim", priority = 1000 },
+    {
+        "folke/which-key.nvim",
         event = "VeryLazy",
         init = function()
             vim.o.timeout = true
@@ -21,50 +22,74 @@ local plugins = {
         end,
         opts = {},
     },
-    {"theprimeagen/harpoon"},
-	{"nvim-telescope/telescope.nvim",
-	    tag = '0.1.1',
-		dependencies = {'nvim-lua/plenary.nvim'}
+    { "theprimeagen/harpoon" },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = '0.1.1',
+        dependencies = { 'nvim-lua/plenary.nvim' }
     },
-	{"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate"
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
     },
-	{
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  dependencies = {
-	    {'neovim/nvim-lspconfig'},             -- Required
-	    {                                      -- Optional
-	        'williamboman/mason.nvim',
-	            build = ":MasonUpdate",
-	    },
-	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
-	    -- Autocompletion
-	    {'hrsh7th/nvim-cmp'},     -- Required
-	    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	    {'L3MON4D3/LuaSnip'},     -- Required
-	  }
-	},
-    {"honza/vim-snippets"},
-    {"dcampos/nvim-snippy"},
-    {"dcampos/cmp-snippy"},
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                                -- Optional
+                'williamboman/mason.nvim',
+                build = ":MasonUpdate",
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'L3MON4D3/LuaSnip' },         -- Required
+        }
+    },
+    { "honza/vim-snippets" },
+    { "dcampos/nvim-snippy" },
+    { "dcampos/cmp-snippy" },
     {
         "kdheepak/lazygit.nvim",
-        dependencies = {"nvim-lua/plenary.nvim"},
+        dependencies = { "nvim-lua/plenary.nvim" },
     },
     {
         'goolord/alpha-nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function ()
-            require'alpha'.setup(require'alpha.themes.startify'.config)
+        config = function()
+            require 'alpha'.setup(require 'alpha.themes.startify'.config)
         end
     },
     {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
-    {'nvim-tree/nvim-tree.lua',
+        'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        'nvim-tree/nvim-tree.lua',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("neorg").setup {
+                load = {
+                    ["core.defaults"] = {}, -- Loads default behaviour
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.dirman"] = { -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/notes",
+                            },
+                        },
+                    },
+                },
+            }
+        end,
     },
 }
 
