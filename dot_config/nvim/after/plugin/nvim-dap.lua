bcaaa0e83b5a0696139e2ac6f13c8a4fc05e3d66
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-06-26 20:47:52 -0300

    Update .config/nvim/after/plugin/nvim-dap.lua

--- dot_config/nvim/after/plugin/nvim-dap.lua
+++ dot_config/nvim/after/plugin/nvim-dap.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint<cr>", { desc = "Toggle breakpoint" })
-vim.keymap.set("n", "F12", "<cmd> DapStepOverStepOver<cr>", { desc = "Step over" })
+vim.keymap.set("n", "<F12>", "<cmd> DapStepOverStepOver<cr>", { desc = "Step over" })
 vim.keymap.set("n", "<leader>dui", function()
     local widgets = require('dap.ui.widgets');
     local sidebar = widgets.sidebar(widgets.scopes);
