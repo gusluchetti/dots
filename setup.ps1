b3115d80df9a4a999e900d1fb7a6829ce4634cc7
Gustavo Luchetti <gustavo@luchetti.dev>
2023-07-11 14:41:14 -0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -30,20 +30,19 @@ scoop bucket add extras
 scoop bucket add versions
 scoop bucket add games
 
-echo "`nInstalling Dependencies..."
 sudo winget install Microsoft.DotNet.DesktopRuntime.7 --architecture x86
-echo "`nInstalling Essentials..."
-sudo scoop install git github 7zip vlc-nightly googlechrome firefox discord --global
-scoop install teamspeak3 obsidian notion spotify-tui neovim foobar2000 anki steamcmd
-echo "`nInstalling Extras..."
-scoop install logitech-omm tinynvidiaupdatechecker obs-studio vscodium 1password-cli
-scoop install windirstat librehardwaremonitor bottom rufus
-echo "`nInstalling Gaming related programs..."
+echo "`nInstalling essentials..."
+sudo scoop install git 7zip vlc steamcmd --global
+scoop install googlechrome firefox github discord teamspeak3 obsidian notion spotify-tui neovim foobar2000 anki
+echo "`nInstalling extras..."
+scoop install tinynvidiaupdatechecker obs-studio vscodium 1password-cli
+scoop install logitech-omm windirstat librehardwaremonitor bottom rufus
+echo "`nInstalling gaming related programs..."
 scoop install yuzu betterjoy vibrance-gui autoclicker archisteamfarm steamcmd
 echo "`nInstalling development things..."
 scoop install touch curl ripgrep sd gcc rustup nodejs pnpm less neofetch
 scoop install docker docker-compose lazydocker lazygit
 
-echo "`nInstalling FiraCode (NerdFont)"
+echo "`nInstalling nerd font (FiraCode)"
 scoop bucket add nerd-fonts
 sudo scoop install FiraCode FiraCode-NF FiraCode-NF-Mono
