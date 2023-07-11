284253fde0a7f4eb9005136a4fe4868efc2fa34d
Gustavo Luchetti <gustavo@luchetti.dev>
2023-07-11 14:21:10 -0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -32,7 +32,7 @@ echo "`nInstalling Dependencies..."
 sudo winget install Microsoft.DotNet.DesktopRuntime.7 --architecture x86
 echo "`nInstalling Essentials..."
 sudo scoop install git github 7zip vlc-nightly googlechrome firefox discord --global
-scoop install teamspeak3 obsidian notion spotify-tui neovim foobar2000 anki
+scoop install teamspeak3 obsidian notion spotify-tui neovim foobar2000 anki steamcmd
 echo "`nInstalling Extras..."
 scoop install logitech-omm tinynvidiaupdatechecker obs-studio vscodium 1password-cli
 scoop install archisteamfarm windirstat autoclicker vibrance-gui librehardwaremonitor bottom
