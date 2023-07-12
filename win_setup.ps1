8e59e638091caa6330838ac058acab3df5558a71
Gustavo Luchetti <gustavo@luchetti.dev>
2023-07-12 01:35:00 -0300

    feat: update windows setup

--- win_setup.ps1
+++ win_setup.ps1
@@ -1,12 +1,12 @@
 # setup exclusive for windows machines
 echo "Hi! Starting setup..."
-echo "`nInstalling Microsoft Essentials..."
+echo "`n Installing WinGet Programs..."
 winget install -e Microsoft.PowerShell
 winget install -e Microsoft.PowerToys
 winget install -e Microsoft.WindowsTerminal
 winget install -e Microsoft.Office
-
 winget install -e Microsoft.DotNet.DesktopRuntime.7 --architecture x86
+
 echo "`nInstalling iCloud (Microsoft Store)..."
 winget install iCloud --id 9PKTQ5699M62
 echo "`nInstalling Steam..."
@@ -21,12 +21,14 @@ irm get.scoop.sh -outfile 'install.ps1'
 Remove-Item install.ps1
 
 echo "`nSetting up Scoop..."
-scoop install sudo git dark
+scoop install sudo
 sudo scoop install aria2 --global
 scoop config aria2-warning-enabled false
 scoop alias rm upgrade
 scoop alias add upgrade 'scoop update *' 'Updates all apps, just like brew or apt'
 
+echo "`nInstalling git and dark..."
+scoop install git dark
 echo "`nAdding 'extras' and 'versions' buckets..."
 scoop bucket add extras
 scoop bucket add versions
@@ -34,10 +36,10 @@ scoop bucket add games
 
 echo "`nInstalling essentials..."
 sudo scoop install 7zip vlc --global
-scoop install chromium github discord notion spotify-tui neovim foobar2000 anki
+scoop install googlechrome github discord notion spotify-tui neovim foobar2000 anki
 
 echo "`nInstalling extras..."
-scoop install googlechrome firefox obsidian teamspeak3 obs-studio vscodium gimp franz
+scoop install firefox obsidian teamspeak3 obs-studio vscodium gimp franz
 scoop install logitech-omm tinynvidiaupdatechecker handbrake-cli qbittorrent@4.5.2 revouninstaller
 scoop install windirstat librehardwaremonitor bottom rufus flameshot msiafterburner
 
@@ -46,7 +48,8 @@ sudo scoop install osulazer ferium --global
 scoop install yuzu betterjoy vibrance-gui autoclicker archisteamfarm steamcmd
 
 echo "`nInstalling development things..."
-scoop install touch curl ripgrep sd gcc rustup nodejs pnpm less neofetch
+sudo scoop install touch curl ripgrep sd less --global
+scoop install neofetch gcc rustup nodejs pnpm
 scoop install docker docker-compose lazydocker lazygit
 
 echo "`nInstalling nerd font (FiraCode)"
