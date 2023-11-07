715e7b066878d1ded6c98f0d58bb0e5d019d131b
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-11-06 21:19:45 -0300

    feat: making files executable

--- .chezmoitemplates/run_once_setup.sh
+++ .chezmoitemplates/run_once_setup.sh
@@ -1,7 +1,7 @@
 {{ if eq .chezmoi.os "darwin" }}
-{{ template "mac_install-pkgs.sh" }}
+    {{ template "mac_install-pkgs.sh" }}
 {{ else if eq .chezmoi.os "linux" }}
-{{ template "linux-fedora_install-pkgs.sh" }}
+    {{ template "linux-fedora_install-pkgs.sh" }}
 {{ else if eq .chezmoi.os "windows" }}
-{{ template "win_install-pkgs.ps1" }}
+    {{ template "win_install-pkgs.ps1" }}
 {{ end }}
