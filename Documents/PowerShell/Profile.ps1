bb79e1abd0e049c94e14872c8e080df4b249030c
Gustavo Luchetti <gustavo@luchetti.dev>
2023-11-13 18:27:16 -0300

    remove unnecessary comments and add chezmoi alias

--- Documents/PowerShell/Profile.ps1
+++ Documents/PowerShell/Profile.ps1
@@ -1,5 +1,6 @@
 oh-my-posh init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/zash.omp.json" | Invoke-Expression
 
+Set-Alias -Name cz -Value chezmoi
 Set-Alias -Name ls -Value eza
 
 Function longAll {eza -la}
