bb5177320237a17f3e951804f8ee42a4e694d68d
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-11-15 12:00:22 -0300

    update nvim tree keymap

--- dot_config/nvim/after/plugin/nvim-tree.lua
+++ dot_config/nvim/after/plugin/nvim-tree.lua
@@ -1,16 +1,19 @@
 local nvimtree = require("nvim-tree");
 
-vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", {desc = "Toggle nvim-tree"})
+vim.keymap.set("n", "<leader>pv",
+    "<cmd>NvimTreeToggle<CR>",
+    { desc = "Toggle [p]roject [v]iew" }
+)
 
 nvimtree.setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    sort_by = "case_sensitive",
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = false,
+    },
 })
