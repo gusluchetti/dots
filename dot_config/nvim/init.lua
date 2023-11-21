a5849d00a4d544188c638fc1f702f27e675a7a40
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-11-21 00:55:54 -0300

    feat: add old opts

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -236,8 +236,9 @@ require('lazy').setup({
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 
--- Set highlight on search
+-- Search options
 vim.o.hlsearch = false
+vim.o.incsearch = true
 
 -- Make (relative) line numbers default
 vim.o.nu = true
@@ -257,8 +258,8 @@ vim.o.mouse = 'a'
 --  See `:help 'clipboard'`
 vim.o.clipboard = 'unnamedplus'
 
--- Enable break indent
 vim.o.breakindent = true
+vim.o.wrap = false;
 
 -- Save undo history
 vim.o.undofile = true
@@ -266,9 +267,11 @@ vim.o.undofile = true
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.o.smartindent = true
 
--- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
+vim.o.scrolloff = 8
+vim.o.isfname:append("@-@")
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -279,6 +282,7 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+vim.o.colorcolumn = "80"
 
 -- [[ Basic Keymaps ]]
 
