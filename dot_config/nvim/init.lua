9a19a5befa99d1e9defa31d9ec7021c1e0199a7f
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-11-22 16:10:15 -0300

    fix: require into import

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -191,13 +191,12 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  require 'custom.options',
-  require 'custom.keymaps',
+  { import = 'custom.options' },
+  { import = 'custom.keymaps' },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
   --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   { import = 'custom.plugins' },
