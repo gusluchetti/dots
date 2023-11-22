3040e233e2904acf5e7917254f8cbca133a7d919
Gustavo Luchetti <gustavo@luchetti.dev>
2023-11-22 09:16:29 -0300

    feat: add biome setup

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -527,6 +527,7 @@ local servers = {
   html = { filetypes = { 'html', 'twig', 'hbs' } },
   rust_analyzer = {},
   tsserver = {},
+  biome = {},
 }
 
 -- Setup neovim lua configuration
@@ -537,9 +538,11 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
+local mason_lspconfig = require('mason-lspconfig')
 
-mason_lspconfig.setup { ensure_installed = vim.tbl_keys(servers) }
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers)
+}
 
 mason_lspconfig.setup_handlers {
   function(server_name)
@@ -551,9 +554,12 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
+-- After setting up mason-lspconfig you may set up servers via lspconfig
+require("lspconfig").biome.setup {}
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
+--
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
