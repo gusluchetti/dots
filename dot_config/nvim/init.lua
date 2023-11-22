4c9daf63aa6fb028396d97cda1ee512c7c97928f
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-11-22 16:13:00 -0300

    fix: move files to lua dir

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -191,8 +191,8 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  { import = 'custom.options' },
-  { import = 'custom.keymaps' },
+  require 'options',
+  require 'keymaps',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
