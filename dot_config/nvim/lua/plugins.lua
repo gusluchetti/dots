f579d528ed628e7405e98b8d4b2768d41d81026b
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-12-17 01:36:36 -0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -25,7 +25,11 @@ return {
     lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("nvim-tree").setup {}
+      require("nvim-tree").setup({
+        update_focused_file = {
+          enable = true,
+        }
+      })
     end,
   },
 
