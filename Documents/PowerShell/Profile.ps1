a6ac3808f250bc662c6438d66da4f3e32cf1d26e
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-12-16 23:49:55 -0300

    Update Documents/PowerShell/Profile.ps1

--- Documents/PowerShell/Profile.ps1
+++ Documents/PowerShell/Profile.ps1
@@ -1,8 +1,1 @@
-$theme = "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/di4am0nd.omp.json"
-oh-my-posh init pwsh --config $theme | Invoke-Expression
-
-Set-Alias -Name cz -Value chezmoi
-Set-Alias -Name ls -Value eza
-
-Function longAll {eza -la}
-Set-Alias -Name l -Value longAll
+Invoke-Expression (&starship init powershell)
