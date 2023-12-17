0a3a77a6a7c10d9d2fac672cc4b8fee2d441ec3b
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-12-17 01:21:22 -0300

    Update .config/nvim/init.lua Remove .config/nvim/lua/custom/plugins/autopairs.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -154,6 +154,21 @@ require('lazy').setup({
   { 'numToStr/Comment.nvim',  opts = {} },
 
   {
+    "windwp/nvim-autopairs",
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require("nvim-autopairs").setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
+  },
+
+  {
     -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
