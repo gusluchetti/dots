84445dfe93dc6e0d3cbee31883dbf9dc20cd54bb
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-12-17 14:02:10 -0300

    Update .config/nvim/lua/plugins.lua Add .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -95,18 +95,6 @@ return {
   },
 
   {
-    'nvim-lualine/lualine.nvim', -- lualine as statusline
-    opts = {
-      options = {
-        icons_enabled = false,
-        component_separators = '|',
-        section_separators = '',
-        theme = 'gruvbox',
-      },
-    },
-  },
-
-  {
     'lukas-reineke/indent-blankline.nvim', -- indentation guides
     main = 'ibl',
     opts = {
---
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  'nvim-lualine/lualine.nvim',   -- lualine as statusline
+  opts = {
+    options = {
+      icons_enabled = false,
+      component_separators = '|',
+      section_separators = '',
+      theme = 'gruvbox',
+    },
+  },
+}
