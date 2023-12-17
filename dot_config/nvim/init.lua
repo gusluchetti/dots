3f6de69f6f9c6dd0fdab1cab37066b3aa432e93e
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-12-17 01:53:06 -0300

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -29,12 +29,13 @@ require('keymaps')
 require('options')
 
 -- [[ Highlight on yank ]]
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
+  group = vim.api.nvim_create_augroup(
+    'YankHighlight',
+    { clear = true }),
   pattern = '*',
 })
 
