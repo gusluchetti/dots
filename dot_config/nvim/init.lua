5e42994f2b155b43e749601a46267c05ecd8e216
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-12-18 01:23:31 -0300

    Update .config/nvim/init.lua Update .config/nvim/lua/plugins/none-ls.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -13,14 +13,14 @@ vim.g.loaded_netrwPlugin = 1
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -30,70 +30,74 @@ require('options')
 
 -- [[ Highlight on yank ]]
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = vim.api.nvim_create_augroup(
-    'YankHighlight',
-    { clear = true }),
-  pattern = '*',
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = vim.api.nvim_create_augroup(
+        'YankHighlight',
+        { clear = true }),
+    pattern = '*',
 })
 
 local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    nmap('<leader>cf', vim.lsp.buf.format, '[C]ode [F]ormat')
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<C-K>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  nmap('<leader>cf', vim.lsp.buf.format, '[C]ode [F]ormat')
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-K>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    -- lsp + telescope
+    local telescope_builtin = require 'telescope.builtin'
+    nmap('gd', telescope_builtin.lsp_definitions, '[G]oto [D]efinition')
+    nmap('gr', telescope_builtin.lsp_references, '[G]oto [R]eferences')
+    nmap('gI', telescope_builtin.lsp_implementations, '[G]oto [I]mplementation')
+    nmap('<leader>D', telescope_builtin.lsp_type_definitions, 'Type [D]efinition')
+    nmap('<leader>ds', telescope_builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', telescope_builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-  -- lsp + telescope
-  local telescope_builtin = require 'telescope.builtin'
-  nmap('gd', telescope_builtin.lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', telescope_builtin.lsp_references, '[G]oto [R]eferences')
-  nmap('gI', telescope_builtin.lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', telescope_builtin.lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', telescope_builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', telescope_builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
 end
 
 require('mason').setup()
 require('mason-lspconfig').setup()
 
 local servers = {
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      diagnostics = { disable = { 'missing-fields' } },
+    lua_ls = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+            diagnostics = { disable = { 'missing-fields' } },
+        },
     },
-  },
-  pyright = {},
-  rust_analyzer = {},
-  tsserver = {},
-  html = { filetypes = { 'html', 'twig', 'hbs' } },
-  clangd = {},
-  -- gopls = {},
+    rust_analyzer = {},
+    tsserver = {},
+    html = { filetypes = { 'html', 'twig', 'hbs' } },
+    clangd = {},
+    gopls = {},
+    -- python tools
+    pyright = {},
+    isort = {},
+    black = {},
+    ruff = {},
 }
 
 local mason_lspconfig = require('mason-lspconfig')
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers)
+    ensure_installed = vim.tbl_keys(servers)
 }
 
 require('neodev').setup()
@@ -102,13 +106,13 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 mason_lspconfig.setup_handlers {
-  function(server_name)
-    local lspconfig = require 'lspconfig'
-    lspconfig[server_name].setup {
-      on_attach = on_attach,
-      settings = servers[server_name],
-      capabilities = capabilities,
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
+    function(server_name)
+        local lspconfig = require 'lspconfig'
+        lspconfig[server_name].setup {
+            on_attach = on_attach,
+            settings = servers[server_name],
+            capabilities = capabilities,
+            filetypes = (servers[server_name] or {}).filetypes,
+        }
+    end,
 }
--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -1,33 +1,32 @@
 return {
-  'nvimtools/none-ls.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    'nvimtools/none-ls.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    null_ls.setup({
-      sources = {
-        -- python
-        null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        -- node.js
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false })
+        null_ls.setup({
+            sources = {
+                -- python
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.diagnostics.ruff,
+            },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
             end,
-          })
-        end
-      end,
 
-    })
-  end
+        })
+    end
 }
