c6b7e0c9eb35db7cf5fd41ba8d2fd4c97df7b4a2
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-12-17 21:06:59 -0300

    Update .config/nvim/lua/plugins/none-ls.lua

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -5,6 +5,7 @@ return {
   },
   config = function()
     local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
     null_ls.setup({
       sources = {
@@ -14,6 +15,19 @@ return {
         null_ls.builtins.formatting.isort,
         -- node.js
       },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+
     })
   end
 }
