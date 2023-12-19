95b1989d2eee66fe2d40b51f65d716dc7305e9b9
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2023-12-18 23:27:17 -0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -166,6 +166,18 @@ return {
         version = "*",
         opts = {
             direction = 'float'
-        }
+        },
+        config = function()
+            local Terminal = require('toggleterm.terminal').Terminal
+            local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
+
+            function _lazygit_toggle()
+                lazygit:toggle()
+            end
+
+            vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>",
+                { noremap = true, silent = true }
+            )
+        end
     }
 }
