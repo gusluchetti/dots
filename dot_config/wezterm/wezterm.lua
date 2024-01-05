2d7afaea35fe0a5c9151dbd672a859a1b3ed79e6
Gustavo Luchetti <gustavo@luchetti.dev>
2024-01-05 10:32:55 -0300

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,28 +1,23 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 local config = {}
-
-local launch_menu = {}
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  table.insert(launch_menu, {
-    label = 'PowerShell 7',
-    args = { 'pwsh.exe' },
-  })
-end
-
-config.launch_menu = launch_menu
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
+
+config.launch_menu = {
+    {
+        label = 'Powershell 7',
+        args = { 'pwsh.exe' }
+    }
+}
 
 config.font_size = 15.0
 config.font = wezterm.font {
-  family = 'BerkeleyMono Nerd Font',
+    family = 'BerkeleyMono Nerd Font',
 }
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 
--- and finally, return the configuration to wezterm
 return config
