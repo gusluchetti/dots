c5f460212bbcf3c4ed22fc7194deef1bf306886f
Gustavo Luchetti <gustavo@luchetti.dev>
2024-01-06 15:37:23 -0300

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ end
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     config.default_prog = { 'pwsh' }
-    config.window_background_image = "C:\\Users\\Gustavo\\extras\\wallpapers\\lockscreen.jpeg"
+    config.window_background_image = "anime.png"
     config.window_background_image_hsb = {
         brightness = 0.5,
         hue = 1.0,
