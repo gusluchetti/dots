3fb8466ad5e2b71ee5f8dd1fdbf729fa5b8596e4
Gustavo Luchetti <gustavo@luchetti.dev>
2024-01-06 15:43:56 -0300

    Update .config/wezterm/wezterm.lua Change attributes of .config/anime.png

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,12 +9,12 @@ end
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     config.default_prog = { 'pwsh' }
-    config.window_background_image = "..\\..\\extras\\wallpapers\\anime.png"
-    config.window_background_image_hsb = {
-        brightness = 0.5,
-        hue = 1.0,
-        saturation = 1.0,
-    }
+    config.window_background_image = "./anime.png"
+    -- config.window_background_image_hsb = {
+    --     brightness = 0.5,
+    --     hue = 1.0,
+    --     saturation = 1.0,
+    -- }
 else
     config.window_background_opacity = 0.9
 end
