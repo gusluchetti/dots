72d0340df0960e83b8b4597735b64c85fb6c1452
Gustavo Luís Luchetti <gustavo@luchetti.dev>
2024-01-06 16:58:51 -0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -4,9 +4,9 @@ return {
     priority = 1000,
     opts = { contrast = "dark" },
     config = function()
-      vim.cmd.colorscheme('gruvbox')
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      vim.cmd("let g:gruvbox_transparent_bg = 1")
+      vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
+      vim.cmd("colorscheme gruvbox")
     end,
   },
 
