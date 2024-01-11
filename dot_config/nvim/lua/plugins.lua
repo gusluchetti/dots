d7a26a6614afe76e2e55c815502ca8a5d12b8fb8
Gustavo Luchetti <gustavo@luchetti.dev>
2024-01-11 00:41:02 -0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -85,21 +85,7 @@ return {
     'lukas-reineke/indent-blankline.nvim', -- indentation guides
     main = 'ibl',
     opts = {
-      indent = {
-        highlight = {
-          "CursorColumn",
-          "Whitespace",
-        },
-        char = ""
-      },
-      whitespace = {
-        highlight = {
-          "CursorColumn",
-          "Whitespace",
-        },
-        remove_blankline_trail = false,
-      },
-      scope = { enabled = false },
+      scope = { enabled = true },
     },
   },
 
