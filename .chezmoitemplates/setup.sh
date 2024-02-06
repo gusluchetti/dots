8c2be66395e6c7f88d64ca281d6084cb03278dd2
Gustavo Luchetti <sushii@prox-debian.lenovo.proxmox.debian>
2024-02-06 13:03:47 -0300

    Update .chezmoitemplates/setup.sh

--- .chezmoitemplates/setup.sh
+++ .chezmoitemplates/setup.sh
@@ -1,16 +1,27 @@
 sudo apt update && sudo apt upgrade
-sudo apt install git gh curl wget unzip gcc clang
-sudo apt install zsh ffmpeg neofetch
+sudo apt install zsh
 zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo apt install git gh curl wget unzip gcc clang
+sudo apt install ffmpeg neofetch
 
 gh auth login # setup git with ssh
 
-# change shell
-chsh -s $(which zsh)
+chsh -s $(which zsh) # change shell
 
 # install asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+# rust
+asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git
+asdf install rust latest
+asdf global rust latest
+# node.js
+sudo apt install dirmngr gpg curl gawk
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs latest
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # install rustup
 cargo install --locked starship zoxide
 
