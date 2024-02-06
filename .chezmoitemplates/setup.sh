5386da4162ed4865f0eeb506afc7c9c0f4a84008
Gustavo Luchetti <sushii@prox-debian.lenovo.proxmox.debian>
2024-02-06 12:53:27 -0300

    Update .chezmoitemplates/setup.sh

--- .chezmoitemplates/setup.sh
+++ .chezmoitemplates/setup.sh
@@ -1,14 +1,16 @@
 sudo apt update && sudo apt upgrade
-sudo apt install git curl wget unzip 7z
-sudo apt install ffmpeg zsh gh neofetch gcc clang
+sudo apt install git gh curl wget unzip gcc clang
+sudo apt install zsh ffmpeg neofetch
+zsh
 
 gh auth login # setup git with ssh
 
 # change shell
 chsh -s $(which zsh)
 
+# install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # install rustup
 cargo install --locked starship zoxide
 
-# install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
