3164db20f3adfaa771d41fe1c2503ddc57aa4b72
Gustavo Luchetti <gustavo@luchetti.dev>
2024-02-06 13:05:16 -0300

    Update .chezmoitemplates/setup.sh

--- .chezmoitemplates/setup.sh
+++ .chezmoitemplates/setup.sh
@@ -25,3 +25,10 @@ asdf install nodejs latest
 
 cargo install --locked starship zoxide
 
+
+
+# lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
