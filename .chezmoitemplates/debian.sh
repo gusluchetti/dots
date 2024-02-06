4cc90335c8bbbd8cfe9349e379569c3870998087
Gustavo Luchetti <gustavo@luchetti.dev>
2024-02-06 14:27:21 -0300

    Update .chezmoitemplates/debian.sh

--- .chezmoitemplates/debian.sh
+++ .chezmoitemplates/debian.sh
@@ -2,15 +2,13 @@
 sudo apt update && sudo apt upgrade
 sudo apt install zsh git
 
-echo "'.chezmoiexternal' installs oh-my-zsh and asdf"
+echo "\n'.chezmoiexternal' installs oh-my-zsh and asdf"
 (
 chsh -s $(which zsh) # change shell
 
 sudo apt install build-essentials net-tools git
 sudo apt install gh curl wget unzip gcc clang
 sudo apt install ffmpeg neofetch
-
-echo "logging in into Github CLI"
 
 if gh auth status | grep -q "Logged in"; then
     echo "already logged on Github CLI"
@@ -19,20 +17,20 @@ else
     gh auth login # interactive
 fi
 
-echo "setting up asdf rust"
+echo "\nsetting up asdf rust"
 # rust + cargo programs
 asdf plugin add rust https://github.com/asdf-community/asdf-rust.git
 asdf install rust latest
 asdf global rust latest
 cargo install --locked starship zoxide
 
-echo "setting up asdf node.js"
+echo "\nsetting up asdf node.js"
 # node.js
 sudo apt install dirmngr gpg curl gawk
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs latest
 
-echo "installing lazygit"
+echo "\ninstalling lazygit"
 # lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -40,7 +38,7 @@ tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 rm -rf lazygit lazygit.tar.gz
 
-echo "installing bun"
+echo "\ninstalling bun"
 # bun
 curl -fsSL https://bun.sh/install | bash
 
