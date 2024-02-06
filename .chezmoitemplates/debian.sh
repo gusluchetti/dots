eb0b1dfe766ce01d87fac11f773cf57c336a532f
Gustavo Luchetti <gustavo@luchetti.dev>
2024-02-06 14:18:30 -0300

    Update .chezmoitemplates/debian.sh

--- .chezmoitemplates/debian.sh
+++ .chezmoitemplates/debian.sh
@@ -10,19 +10,23 @@ sudo apt install build-essentials net-tools git
 sudo apt install gh curl wget unzip gcc clang
 sudo apt install ffmpeg neofetch
 
+echo "logging in into Github CLI"
 gh auth login # setup git with ssh
 
+echo "setting up asdf rust"
 # rust + cargo programs
 asdf plugin add rust https://github.com/asdf-community/asdf-rust.git
 asdf install rust latest
 asdf global rust latest
 cargo install --locked starship zoxide
 
+echo "setting up asdf node.js"
 # node.js
 sudo apt install dirmngr gpg curl gawk
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs latest
 
+echo "installing lazygit"
 # lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -30,6 +34,7 @@ tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 rm -rf lazygit lazygit.tar.gz
 
+echo "installing bun"
 # bun
 curl -fsSL https://bun.sh/install | bash
 
