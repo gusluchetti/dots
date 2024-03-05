f0d6e44628c840730425253c421282045c2e0eb6
Gustavo Luchetti <gustavo@luchetti.dev>
2024-03-05 10:31:39 -0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,161 +1,167 @@
 return {
-    {
-        'ellisonleao/gruvbox.nvim',
-        priority = 1000,
-        opts = { contrast = "dark" },
-        config = function()
-            vim.cmd("let g:gruvbox_transparent_bg = 1")
-            vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
-            vim.cmd("colorscheme gruvbox")
-        end,
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    opts = { contrast = "dark" },
+    config = function()
+      vim.cmd("let g:gruvbox_transparent_bg = 1")
+      vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
+
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require 'colorizer'.setup({
+        html = { mode = 'background' },
+      }, { css = true })
+    end,
+  },
+
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'folke/neodev.nvim',
+      { 'j-hui/fidget.nvim', opts = {} },
     },
+  },
 
-    {
-        'norcalli/nvim-colorizer.lua',
-        config = function()
-            require 'colorizer'.setup({
-                html = { mode = 'background' },
-            }, { css = true })
-        end,
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        update_focused_file = {
+          enable = true,
+        }
+      })
+    end,
+  },
+
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk,
+          { buffer = bufnr, desc = '[Git] Hunk [P]review' }
+        )
+        -- don't override the built-in and fugitive keymaps
+        local gs = package.loaded.gitsigns
+        vim.keymap.set({ 'n', 'v' }, ']c', function()
+          if vim.wo.diff then
+            return ']c'
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+        vim.keymap.set({ 'n', 'v' }, '[c', function()
+          if vim.wo.diff then
+            return '[c'
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+      end,
     },
+  },
 
-    {
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
-            'folke/neodev.nvim',
-            { 'j-hui/fidget.nvim', opts = {} },
-        },
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
+
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end
+  },
+
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'nvim-telescope/telescope.nvim',
+      'mfussenegger/nvim-dap-python'
     },
-
-    {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        lazy = false,
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("nvim-tree").setup({
-                update_focused_file = {
-                    enable = true,
-                }
-            })
-        end,
+    opts = {
+      auto_refresh = true
     },
-
-    {
-        'lewis6991/gitsigns.nvim',
-        opts = {
-            signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-            },
-            on_attach = function(bufnr)
-                vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk,
-                    { buffer = bufnr, desc = '[Git] Hunk [P]review' }
-                )
-                -- don't override the built-in and fugitive keymaps
-                local gs = package.loaded.gitsigns
-                vim.keymap.set({ 'n', 'v' }, ']c', function()
-                    if vim.wo.diff then
-                        return ']c'
-                    end
-                    vim.schedule(function()
-                        gs.next_hunk()
-                    end)
-                    return '<Ignore>'
-                end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
-                vim.keymap.set({ 'n', 'v' }, '[c', function()
-                    if vim.wo.diff then
-                        return '[c'
-                    end
-                    vim.schedule(function()
-                        gs.prev_hunk()
-                    end)
-                    return '<Ignore>'
-                end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
-            end,
-        },
+    event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { '<leader>vs', '<cmd>VenvSelect<cr>' },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
     },
+  },
 
-    -- "gc" to comment visual regions/lines
-    { 'numToStr/Comment.nvim', opts = {} },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependecies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon.setup()
 
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        config = true
-        -- use opts = {} for passing setup options
-        -- this is equalent to setup({}) function
-    },
+      vim.keymap.set("n", "<leader>ha",
+        function() harpoon:list():append() end,
+        { desc = '[H]arpoon [A]dd' })
 
-    {
-        'windwp/nvim-ts-autotag',
-        config = function()
-            require('nvim-ts-autotag').setup()
-        end
-    },
+      vim.keymap.set("n", "<C-e>",
+        function() harpoon.ui:toggle_quick_menu(harpoon:list()) end
+        , { desc = 'Toggle Harpoon Menu' })
 
-    {
-        'linux-cultist/venv-selector.nvim',
-        dependencies = {
-            'neovim/nvim-lspconfig',
-            'nvim-telescope/telescope.nvim',
-            'mfussenegger/nvim-dap-python'
-        },
-        opts = {
-            auto_refresh = true
-        },
-        event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-        keys = {
-            -- Keymap to open VenvSelector to pick a venv.
-            { '<leader>vs', '<cmd>VenvSelect<cr>' },
-            -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-            { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-        },
-    },
+      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+    end
+  },
 
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependecies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local harpoon = require 'harpoon'
-            harpoon.setup()
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {},
+    config = function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit  = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
 
-            vim.keymap.set("n", "<leader>ha",
-                function() harpoon:list():append() end,
-                { desc = '[H]arpoon [A]dd' })
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
 
-            vim.keymap.set("n", "<C-e>",
-                function() harpoon.ui:toggle_quick_menu(harpoon:list()) end
-                , { desc = 'Toggle Harpoon Menu' })
+      vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>",
+        { noremap = true, silent = true }
+      )
+    end
+  },
 
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-        end
-    },
-
-    {
-        'akinsho/toggleterm.nvim',
-        version = "*",
-        opts = {},
-        config = function()
-            local Terminal = require('toggleterm.terminal').Terminal
-            local lazygit  = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
-
-            function _lazygit_toggle()
-                lazygit:toggle()
-            end
-
-            vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>",
-                { noremap = true, silent = true }
-            )
-        end
-    }
+  {
+    "davidmh/mdx.nvim",
+    config = true,
+    dependencies = { "nvim-treesitter/nvim-treesitter" }
+  }
 }
