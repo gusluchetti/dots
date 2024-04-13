27cadc67bfc456974da2f9388bdf3a0779aa63a8
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-13 16:14:45 -0300

    Update .config/nvim/init.lua Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,8 +8,8 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- disable netrw, since we're using nvim-tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -29,6 +29,20 @@ return {
   },
 
   {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        update_focused_file = {
+          enable = true,
+        }
+      })
+    end,
+  },
+
+  {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
