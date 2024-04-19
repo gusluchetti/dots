2de7dfbbb02b8e811031bc4a529fd196c068344c
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-19 17:04:31 -0300

    Update .config/nvim/lua/plugins.lua Remove .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -20,6 +20,18 @@ return {
   },
 
   {
+    'nvim-lualine/lualine.nvim', -- lualine as statusline
+    opts = {
+      options = {
+        icons_enabled = false,
+        section_separators = '',
+        component_separators = '-',
+        theme = 'gruvbox',
+      },
+    },
+  },
+
+  {
     'neovim/nvim-lspconfig',
     dependencies = {
       'williamboman/mason.nvim',
