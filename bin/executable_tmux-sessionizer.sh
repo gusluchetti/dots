3819a28f24689be5b24e37775ace5b3c22c717cd
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-24 13:50:29 -0300

    fix: tmux sessionizer
    understanding attaching as opposed to switching clients. should work now.

--- bin/executable_tmux-sessionizer.sh
+++ bin/executable_tmux-sessionizer.sh
@@ -11,16 +11,15 @@ if [[ -z $s_path ]]; then
 fi
 
 s_name=$(basename "$s_path" | tr . _)
-tmux_running=$(pgrep tmux)
 
-if [[ -z $tmux_running ]]; then
+if [[ -z $TMUX ]]; then
     tmux new-session -s $s_name -c $s_path
     exit 0
 fi
 
 if ! tmux has-session -t $s_name 2> /dev/null; then
     tmux new-session -ds $s_name -c $s_path
+    tmux switch-client -t $s_name
 fi
 
-# either goes to client, or attaches to tmux
-tmux attach -dt $s_name
+tmux attach -t $s_name
