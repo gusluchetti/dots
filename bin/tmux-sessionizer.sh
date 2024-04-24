b9bd7c583203d7849307d48caae65c359dd240d2
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-24 10:52:44 -0300

    feat: add tmux-sessionizer script
    heavily based off primeagen's script, with a couple modifications
    to finding project logic and fzf args

---
+++ bin/tmux-sessionizer.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 1 ]]; then
+    s_path=$1
+else
+    s_path=$(find * -name '.git' | xargs -I{} dirname {} | fzf --height 50% --reverse)
+fi
+
+if [[ -z $s_path ]]; then
+    exit 0
+fi
+
+s_name=$(basename "$s_path" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $s_name -c $s_path
+    exit 0
+fi
+
+if ! tmux has-session -t=$s_name 2> /dev/null; then
+    tmux new-session -ds $s_name -c $s_path
+fi
+
+tmux switch-client -t $s_name
