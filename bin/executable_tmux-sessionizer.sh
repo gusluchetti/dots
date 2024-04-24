bf2e0f2e4af8eb3d99a86cbf92c721393f3b4e58
Gustavo Luchetti <gustavo@luchetti.dev>
2024-04-24 13:59:09 -0300

    Update bin/tmux-sessionizer.sh

--- bin/executable_tmux-sessionizer.sh
+++ bin/executable_tmux-sessionizer.sh
@@ -11,15 +11,20 @@ if [[ -z $s_path ]]; then
 fi
 
 s_name=$(basename "$s_path" | tr . _)
+tmux_running=$(pgrep tmux)
 
-if ! [[ -z $TMUX ]]; then
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
     tmux new-session -s $s_name -c $s_path
     exit 0
 fi
 
 if ! tmux has-session -t $s_name 2> /dev/null; then
     tmux new-session -ds $s_name -c $s_path
+fi
+
+if [[ -z $TMUX ]]; then
+    tmux new-session -A -t $s_name
+else
     tmux switch-client -t $s_name
 fi
 
-tmux attach -t $s_name
