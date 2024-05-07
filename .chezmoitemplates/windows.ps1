b522da66930a10f8e8d15924c6cbb8c40783b3ff
Gustavo Luchetti <gustavo@luchetti.dev>
2024-05-06 21:52:44 -0300

    fix: change comment position

--- .chezmoitemplates/windows.ps1
+++ .chezmoitemplates/windows.ps1
@@ -1,21 +1,18 @@
-# microsoft things
+# sudo for windows
 winget install gerardog.gsudo --force
-
+# microsoft things
 sudo winget install --silent --accept-package-agreements --accept-source-agreements `
 --disable-interactivity --verbose Microsoft.PowerShell Microsoft.Office `
 Microsoft.PowerToys Microsoft.VisualStudio.2022.BuildTools Microsoft.Teams `
-Microsoft.OpenSSH.Beta `
-# main
+Microsoft.OpenSSH.Beta ` # main
 Git.Git GitHub.cli Neovim.Neovim SyncTrayzor.SyncTrayzor 7zip.7zip `
 VideoLAN.VLC Anki.Anki Klocman.BulkCrapUninstaller MoritzBunkus.MKVToolNix `
 AntibodySoftware.WizTree AntibodySoftware.WizFile HandBrake.HandBrake.CLI `
 Obsidian.Obsidian Wagnardsoft.DisplayDriverUninstaller OBSProject.OBSStudio `
-Guru3D.Afterburner Guru3D.RTSS OPAutoClicker.OPAutoClicker Logitech.OnboardMemoryManager
-# gaming
+Guru3D.Afterburner Guru3D.RTSS OPAutoClicker.OPAutoClicker Logitech.OnboardMemoryManager ` # gaming
 Valve.Steam Discord.Discord FACEITLTD.FACEITAC `
-TeamSpeakSystems.TeamSpeakClient.Beta Peppy.Osu!`
-# misc
-WhatsApp.WhatsApp.Beta Mozilla.Firefox.DeveloperEdition Amazon.Kindle`
+TeamSpeakSystems.TeamSpeakClient.Beta Peppy.Osu! ` # misc
+WhatsApp.WhatsApp.Beta Mozilla.Firefox.DeveloperEdition Amazon.Kindle `
 PeterPawlowski.foobar2000 PeterPawlowski.foobar2000.EncoderPack
 
 echo "`nA restart is necessary finish installation..." 
