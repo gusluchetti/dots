2e5dec8a08e21954c7e17b5357ba36d4338b841c
Gustavo Luchetti <gustavo@luchetti.dev>
2024-05-13 21:44:07 -0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -14,7 +14,7 @@ return {
     'brenoprata10/nvim-highlight-colors',
     config = function()
       require("nvim-highlight-colors").setup {
-        render = 'background',
+        render = 'virtual',
         virtual_symbol = '■',
         enable_named_colors = true,
         enable_tailwind = true,
