4b010f69fda97b4ecfe16646fe742740559fc8e5
Gustavo Luchetti <gustavo@luchetti.dev>
2024-05-16 22:14:53 -0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -90,8 +90,8 @@ return {
         javascript = { { "prettierd", "prettier" } },
         markdown = { { "prettierd", "prettier" } },
       },
-      -- Set up format-on-save
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      -- TODO: Set up format-on-save
+      -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
       -- Customize formatters
       formatters = {
         shfmt = {
