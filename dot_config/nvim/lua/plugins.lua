314a94c5493ff469d55e840c9173f7cfd3ba0d35
Gustavo Luchetti <gustavo@luchetti.dev>
2024-05-16 22:21:20 -0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -11,6 +11,10 @@ return {
   },
 
   {
+    'rebelot/kanagawa.nvim',
+  },
+
+  {
     'brenoprata10/nvim-highlight-colors',
     config = function()
       require("nvim-highlight-colors").setup {
