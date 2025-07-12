9a98bf3908b8f0a6574c52f3f0cf00436b9876cc
Gustavo Luchetti <gustavo@luchetti.dev>
2025-07-12 15:55:53 -0300

    Update .chezmoitemplates/arch.sh

--- .chezmoitemplates/arch.sh
+++ .chezmoitemplates/arch.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
 pkgs=(
-  # core
-  base-devel # dependency for most things
-  net-tools # collecting of networking programs
-  zsh # better than bash
-  curl # when wget is not enough
-  wget # when curl is not enough
-  xclip # clipboard manager
-  man-db # online manual database
-  ffmpeg # convert/stream media
-  unzip # unzip files
+  # core/deps
+  base-devel
+  net-tools
+  zsh
+  curl
+  wget
+  man-db
+  unzip
+  ffmpeg
 
   # dev
-  neovim # THE text editor
-  lazygit # git inside neovim (for lazy people)
-  tmux # terminal multiplexer
-  mise # manages envs, dev programs, etc.
-
-  # misc
-  fzf # fuzzy finder
-  eza # modern ls replacement
-  findutils # find things!
-  ripgrep # grep but blazingly fast
-  tldr # too long, didn't read man pages
-
-  # finances
-  hledger # manage your finances
-  hledger-ui # TUI for hledger
-  hledger-web # web interface for hledger
-  hledger-iadd # interactively create hledger entries
+  neovim
+  lazygit
+  tmux
+  mise
+  fzf
+  eza
+  findutils
+  ripgrep
 )
 
 # Ask the user for the environment (server or desktop)
@@ -39,17 +29,15 @@ read env
 # If desktop, append desktop-specific packages
 if [[ "$env" == "desktop" ]]; then
     pkgs+=(
-	  networkmanager # manages something?
-	  i3 # lean window manager
-	  rofi # app launcher
-	  pipewire # audio manager
-	  dunst # lightweight notification daemon
-	  vlc # video player
-	  firefox-developer-edition # firefox dev edition
-	  flameshot # screenshot tool
-	  mpv # another useful media player
-	  alacritty # terminal emulator
-	  rate-mirrors-bin # rating arch package mirrors
+	  i3
+	  alacritty
+	  networkmanager
+	  pipewire # audio
+	  vlc
+    firefox
+    flameshot
+	  rofi # action launcher
+	  dunst # notification daemon
 	  udiskie # removable media automounter
     )
 fi
