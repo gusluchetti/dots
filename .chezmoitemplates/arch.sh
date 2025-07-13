adac937c0346adbf05360fb2389ef896c58f593a
Gustavo Luchetti <gustavo@luchetti.dev>
2025-07-13 12:33:19 -0300

    feat: updating arch setup script

--- .chezmoitemplates/arch.sh
+++ .chezmoitemplates/arch.sh
@@ -11,6 +11,7 @@ pkgs=(
   man-db
   unzip
   ffmpeg
+  mpv
 
   # dev
   neovim
@@ -22,37 +23,43 @@ pkgs=(
   ripgrep
 )
 
-# Ask the user for the environment (server or desktop)
+# ask the user for environment type (server or desktop)
 echo "Select the environment (server/desktop):"
 read env
 
-# If desktop, append desktop-specific packages
+# append desktop-specific packages (mostly GUI and others)
 if [[ "$env" == "desktop" ]]; then
     pkgs+=(
-	  i3
-	  alacritty
-	  networkmanager
-	  vlc
-    firefox
-    flameshot
-	  pipewire # audio
-	  rofi # action launcher
-	  dunst # notification daemon
-	  udiskie # removable media automounter
+      i3
+      alacritty
+      networkmanager
+      vlc
+      firefox
+      flameshot
+      pipewire # audio
+      rofi # action launcher
+      dunst # notification daemon
+      udiskie # removable media automounter
     )
 fi
 
+# setting up yay, using it from now on
 if ! [ -x "$(type -p yay)" ]; then
-  # building yay from source
   sudo pacman -S --needed git base-devel
   git clone https://aur.archlinux.org/yay.git 
   cd yay 
   makepkg -si
   cd .. && rm -rf yay/
-fi
-
-if ! [ -x "$(type -p yay)" ]; then
-  curl https://mise.run | MISE_INSTALL_PATH=~/.local/bin/mise sh
+  yay
 fi
 
 yay -S --noconfirm "${pkgs[@]}"
+
+# changing shell to zsh
+chsh -s /usr/bin/zsh
+rm -rf ~/.bash*
+
+if ! [ -x "$(type -p mise)" ]; then
+  curl https://mise.run | MISE_INSTALL_PATH=~/.local/bin/mise sh
+  mise install
+fi
