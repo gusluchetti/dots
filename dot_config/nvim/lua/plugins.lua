3b33fe0a04f32211b1d6d8efba35efe149bc46cc
Gustavo Luchetti | Nitro <gustavo.luchetti@nitro.com.br>
2025-07-14 11:05:52 -0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -165,27 +165,18 @@ return {
 		"folke/which-key.nvim",
 		event = "VimEnter", -- Sets the loading event to 'VimEnter'
 		config = function() -- This is the function that runs, AFTER loading
-			require("which-key").setup()
+			local wk = require("which-key")
+			wk.setup()
 
 			-- Document existing key chains
-			require("which-key").register({
+			wk.add({
 				{ "<leader>c", group = "[C]ode" },
-				{ "<leader>c_", hidden = true },
 				{ "<leader>d", group = "[D]ocument" },
-				{ "<leader>d_", hidden = true },
 				{ "<leader>h", group = "Git [H]unk" },
-				{ "<leader>h_", hidden = true },
 				{ "<leader>r", group = "[R]ename" },
-				{ "<leader>r_", hidden = true },
 				{ "<leader>s", group = "[S]earch" },
-				{ "<leader>s_", hidden = true },
 				{ "<leader>t", group = "[T]oggle" },
-				{ "<leader>t_", hidden = true },
 				{ "<leader>w", group = "[W]orkspace" },
-				{ "<leader>w_", hidden = true },
-			})
-			-- visual mode
-			require("which-key").register({
 				{ "<leader>h", desc = "Git [H]unk", mode = "v" },
 			})
 		end,
