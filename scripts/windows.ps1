9ce13cbc5e98be537aa250d09fee5a3cf4e58120
Gustavo Luchetti <gustavo@luchetti.dev>
2026-03-24 23:14:39 -0300

    fix: remove unnecessary programs on windows script

--- scripts/windows.ps1
+++ scripts/windows.ps1
@@ -2,23 +2,22 @@
 winget install --silent --accept-package-agreements --accept-source-agreements `
 --disable-interactivity Microsoft.PowerShell Microsoft.Office `
 Microsoft.PowerToys Microsoft.VisualStudio.2022.BuildTools Microsoft.Teams `
-Microsoft.OpenSSH.Beta
+Microsoft.OpenSSH.Preview
 
 # main
 winget install --silent --accept-package-agreements --accept-source-agreements `
 --disable-interactivity gerardog.gsudo aria2.aria2 Git.Git GitHub.cli `
-Neovim.Neovim SyncTrayzor.SyncTrayzor 7zip.7zip VideoLAN.VLC Anki.Anki `
+Neovim.Neovim 7zip.7zip VideoLAN.VLC Anki.Anki `
 Klocman.BulkCrapUninstaller MoritzBunkus.MKVToolNix AntibodySoftware.WizTree `
 AntibodySoftware.WizFile HandBrake.HandBrake.CLI Obsidian.Obsidian `
 Wagnardsoft.DisplayDriverUninstaller OBSProject.OBSStudio Guru3D.Afterburner `
-Guru3D.RTSS OPAutoClicker.OPAutoClicker Logitech.OnboardMemoryManager `
-IrfanSkiljan.IrfanView
+Guru3D.RTSS OPAutoClicker.OPAutoClicker Logitech.OnboardMemoryManager IrfanSkiljan.IrfanView
 
 # gaming and others
 winget install --silent --accept-package-agreements --accept-source-agreements `
 --disable-interactivity Valve.Steam Discord.Discord FACEITLTD.FACEITAC Peppy.Osu! `
-TeamSpeakSystems.TeamSpeakClient.Beta WhatsApp.WhatsApp.Beta `
-Mozilla.Firefox.DeveloperEdition Amazon.Kindle PeterPawlowski.foobar2000 `
-PeterPawlowski.foobar2000.EncoderPack
+TeamSpeakSystems.TeamSpeakClient WhatsApp.WhatsApp `
+Mozilla.Firefox.DeveloperEdition Amazon.Kindle `
+PeterPawlowski.foobar2000 PeterPawlowski.foobar2000.EncoderPack
 
 echo "`nWindows app installation complete. A restart may be needed."
