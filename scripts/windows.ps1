b414b3199b98f7a7076ea45217133f25c69ed46b
Gustavo Luchetti <gustavo@luchetti.dev>
2026-07-07 16:21:12 -0300

    Update scripts/windows.ps1

--- scripts/windows.ps1
+++ scripts/windows.ps1
@@ -12,13 +12,13 @@ winget install --silent --accept-package-agreements --accept-source-agreements `
 Neovim.Neovim 7zip.7zip VideoLAN.VLC Anki.Anki Klocman.BulkCrapUninstaller `
 MoritzBunkus.MKVToolNix AntibodySoftware.WizTree AntibodySoftware.WizFile `
 HandBrake.HandBrake.CLI Obsidian.Obsidian Wagnardsoft.DisplayDriverUninstaller `
-OBSProject.OBSStudio Guru3D.Afterburner Guru3D.RTSS MatthiasZronek.BenchMate
+OBSProject.OBSStudio Guru3D.Afterburner Guru3D.RTSS MatthiasZronek.BenchMate `
 OPAutoClicker.OPAutoClicker Logitech.OnboardMemoryManager IrfanSkiljan.IrfanView `
 xanderfrangos.twinkletray
 
 # misc
 winget install --silent --accept-package-agreements --accept-source-agreements `
---disable-interactivity Valve.Steam Discord.Discord FACEITLTD.FACEITAC ppy.osu `
+--disable-interactivity Valve.Steam smartfrigde.Legcord FACEITLTD.FACEITAC ppy.osu `
 Mozilla.Firefox PeterPawlowski.foobar2000 PeterPawlowski.foobar2000.EncoderPack
 
 echo "`nWindows app installation complete. A restart may be needed."
